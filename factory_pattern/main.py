c4ccc4c7af52df4ea37e60b9f3eb6c02e155f5c8
martyna <codingbee77@tlen.pl>
2023-03-02 13:29:54 +0100

    Update car names in main.py

--- factory_pattern/main.py
+++ factory_pattern/main.py
@@ -3,7 +3,7 @@ from autofactory import AutoFactory
 
 if __name__ == "__main__":
     factory = AutoFactory()
-    for car_name in "Chevy", "Ford", "Jeep", "Tesla":
+    for car_name in "ChevyVolt", "FordFusion", "JeepSahara", "Tesla Roadster":
         car = factory.create_instance(car_name)
         car.start()
         car.stop()
