5335e1e45422ba7cd837f5e68aac3a6cc1f30078
martyna <codingbee77@tlen.pl>
2023-03-02 11:36:00 +0100

    Refactor main file to use AutoFactory class

--- factory_pattern/main.py
+++ factory_pattern/main.py
@@ -1,22 +1,9 @@
-from chevyvolt import ChevyVolt
-from fordfusion import FordFusion
-from jeepsahara import JeepSahara
-from nullcar import NullCar
-
-
-def get_car(car_name):
-    if car_name == "Chevy" or car_name == "Chevrolet":
-        return ChevyVolt()
-    elif car_name == "Ford":
-        return FordFusion()
-    elif car_name == "Jeep Sahara":
-        return JeepSahara()
-    else:
-        return NullCar(car_name)
+from autofactory import AutoFactory
 
 
 if __name__ == "__main__":
+    factory = AutoFactory()
     for car_name in "Chevy", "Ford", "Jeep", "Tesla":
-        car = get_car(car_name)
+        car = factory.create_instance(car_name)
         car.start()
         car.stop()
