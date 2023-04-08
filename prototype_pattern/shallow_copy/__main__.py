17af1d99fe1f519b4726de04799f91f48cd47683
martyna <codingbee77@tlen.pl>
2023-04-08 12:50:35 +0200

    Add tower component

--- prototype_pattern/shallow_copy/__main__.py
+++ prototype_pattern/shallow_copy/__main__.py
@@ -2,9 +2,14 @@ from laptop import Laptop
 from tower import Tower, MainBoard
 
 l1 = Laptop("L1", "Intel", "32GB", "2TB SSD", "onboard", "1920x1080")
-l1.display()
+# print(str(l1))
 l2 = l1.clone()
 l2.model = "L2"
 l2.processor = "AMD"
 l2.display()
 
+t1 = Tower("T1", MainBoard("Asus","Game"), "AMD", "32GB", "2TB SSD", "onboard", "1920x1080")
+t1.display()
+t2 = t1.clone()
+t2.mainboard.model = "Business"
+t2.display()
--- prototype_pattern/shallow_copy/tower.py
+++ prototype_pattern/shallow_copy/tower.py
@@ -3,26 +3,34 @@ from abc_prototype import AbsPrototype
 import copy
 
 
-class Laptop(AbsComputer, AbsPrototype):
-    def __init__(self, model, processor, memory, hard_drive, graphics, screen):
+class MainBoard(object):
+    manufacturer: str
+    model: str
+
+    def __init__(self, manufacturer, model):
+        self.manufacturer = manufacturer
         self.model = model
+
+
+class Tower(AbsComputer, AbsPrototype):
+    def __init__(self, model, mainboard, processor, memory, hard_drive,
+                 graphics, monitor):
+        self.model = model
+        self.mainboard = mainboard
         self.processor = processor
         self.memory = memory
         self.hard_drive = hard_drive
         self.graphics = graphics
-        self.screen = screen
-
-    def __str__(self):
-        return f"Custom Computer: {self.model} \n Processor: {self.processor} \n Memory: {self.memory}" \
-               f" \n Hard drive: {self.hard_drive} \n Graphics: {self.graphics} \n Screen: {self.screen}"
+        self.monitor = monitor
 
     def display(self):
         print(f"Custom Computer: {self.model}")
+        print(f"Mainboard: {self.mainboard.model}")
         print(f"Processor: {self.processor}")
         print(f"Memory: {self.memory}")
         print(f"Hard drive: {self.hard_drive}")
         print(f"Graphics: {self.graphics}")
-        print(f"Screen: {self.screen}")
+        print(f"Monitor: {self.monitor if self.monitor else 'None'}")
 
     def clone(self):
         return copy.copy(self)
