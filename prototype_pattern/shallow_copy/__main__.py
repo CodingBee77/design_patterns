4bef5c496aedfe822b3b8105deca3d08afb8b9e5
martyna <codingbee77@tlen.pl>
2023-04-08 13:10:16 +0200

    Add prototype_manager and use it in __main__.py

--- prototype_pattern/shallow_copy/__main__.py
+++ prototype_pattern/shallow_copy/__main__.py
@@ -1,15 +1,19 @@
 from laptop import Laptop
 from tower import Tower, MainBoard
+from prototype_manager import PrototypeManager
 
+manager = PrototypeManager()
 l1 = Laptop("L1", "Intel", "32GB", "2TB SSD", "onboard", "1920x1080")
-# print(str(l1))
-l2 = l1.clone()
+l1.display()
+manager = {'L1': l1}
+l2 = manager['L1'].clone()
 l2.model = "L2"
 l2.processor = "AMD"
 l2.display()
 
 t1 = Tower("T1", MainBoard("Asus","Game"), "AMD", "32GB", "2TB SSD", "onboard", "1920x1080")
 t1.display()
-t2 = t1.clone()
+manager = {'T1': t1}
+t2 = manager['T1'].clone()
 t2.mainboard.model = "Business"
 t2.display()
