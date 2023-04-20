fc1f2c5bb07a3fe7ed0dfe5365b477dd9de3c9ed
martyna <codingbee77@tlen.pl>
2023-04-20 12:03:02 +0200

    Add VendAdapter class

---
+++ adapter_pattern/vend_adapter.py
@@ -0,0 +1,10 @@
+from abs_adapter import AbsAdapter
+
+class VendAdapter(AbsAdapter):
+    @property
+    def name(self):
+        return self.adaptee.name
+
+    @property
+    def address(self):
+        return f"{self.adaptee.number} {self.adaptee.street}"
