15cf0eaa8a13c56b3c21ebe7d8ee281f3d5b8419
martyna <codingbee77@tlen.pl>
2023-04-20 12:02:23 +0200

    Add main.py file

---
+++ adapter_pattern/__main__.py
@@ -0,0 +1,12 @@
+from mock_vendors import MOCKVENDORS
+
+CUSTOMERS = MOCKVENDORS
+
+
+def main():
+    for customer in CUSTOMERS:
+        print(f"Name {customer.name}; Address {customer.address}")
+
+
+if __name__ == "__main__":
+    main()
