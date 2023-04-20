5b9db813b2db161d132670f98c09130c212c15b3
martyna <codingbee77@tlen.pl>
2023-04-20 12:34:39 +0200

    Adjust mock_vendors.py to pass arguments directly

--- adapter_pattern/mock_vendors.py
+++ adapter_pattern/mock_vendors.py
@@ -1,8 +1,8 @@
 from vendor import Vendor
-from vend_adapter import VendAdapter
+from vend_adapter import VendorAdapter
 
 MOCKVENDORS = (
-    VendAdapter(Vendor("Dough Factory", 1, "Semolina Court")),
-    VendAdapter(Vendor("Farm Produce", 14, "Country Rd.")),
-    VendAdapter(Vendor("Cocoa World", 53, "Tropical Blvd.")),
+    VendorAdapter("Dough Factory", 1, "Semolina Court"),
+    VendorAdapter("Farm Produce", 14, "Country Rd."),
+    VendorAdapter("Cocoa World", 53, "Tropical Blvd."),
 )
